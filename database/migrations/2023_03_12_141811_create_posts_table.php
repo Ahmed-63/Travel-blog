8097a7664ced409bcde1cdb097f1cd176dfe1d4d
Ahmed-63 <ahmed63djani@gmail.com>
2023-08-12 12:34:03 +0200

    correction2

--- database/migrations/2023_03_12_141811_create_posts_table.php
+++ database/migrations/2023_03_12_141811_create_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('picture');
             $table->text('content');
             $table->timestamps();
+            
         });
     }
 
